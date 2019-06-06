682df2f0be1c2db6606415060e858730f2594185
SiriIndukuri <51137084+SiriIndukuri@users.noreply.github.com>
2019-06-06 18:10:24 -0500

    Add files via upload

---
+++ hashmap.java
@@ -0,0 +1,17 @@
+package pyramidtest;
+import java.util.*;  
+class hashmap{  
+	public static void main(String args[]){  
+		HashMap<Integer,String> hm=new HashMap<Integer,String>();    
+		System.out.println("Initial list of elements: "+hm);  
+		hm.put(100,"Amit");    
+		hm.put(101,"Vijay"); 
+		hm.put(101, "value");
+		hm.put(102,"Rahul");   
+
+		System.out.println("After invoking put() method ");  
+		for(Map.Entry m:hm.entrySet()){    
+			System.out.println(m.getKey()+" "+m.getValue());    
+		}  
+	}  
+}  
